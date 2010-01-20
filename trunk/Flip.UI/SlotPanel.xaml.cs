8c36a9442c25a9e99a82588bf46c22e8b361d8ab
keiron.nicholson <keiron.nicholson@81157064-3570-11de-a9fe-ad1e22fc3a8a>
2010-01-20 16:18:54 +0000

    Added a rough attempt at moveable blocks, some of which can be placed in slots in other blocks.

---
+++ trunk/Flip.UI/SlotPanel.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sussex.Flip.UI
+{
+    /// <summary>
+    /// Interaction logic for SlotPanel.xaml
+    /// </summary>
+
+    public partial class SlotPanel : UserControl
+    {
+        public SlotPanel()
+        {
+            InitializeComponent();            
+            Drop += new DragEventHandler(DroppedOnSlotPanel);
+        }
+        
+
+        private void DroppedOnSlotPanel(object sender, DragEventArgs e)
+        {
+			IDataObject data = e.Data;
+			if (data.GetDataPresent(typeof(Moveable))) {
+				Moveable moveable = (Moveable)data.GetData(typeof(Moveable));
+				if (moveable is SimpleBlock) {
+					SetSlotContents((SimpleBlock)moveable);
+				}
+			}
+        }
+
+        
+        public SimpleBlock EmptySlot()
+        {
+        	SimpleBlock removing = (SimpleBlock)SlotBorder.Child;
+        	SlotBorder.Child = null;
+        	return removing;
+        }
+        
+        
+        public void SetSlotContents(SimpleBlock block)
+        {    	
+        	if (GetSlotContents() == block) return;
+        	
+        	if (block != null) {
+        		block.Detach();
+        	}
+        	
+        	SlotBorder.Child = block;
+        }
+        
+        
+        public SimpleBlock GetSlotContents()
+        {
+        	if (SlotBorder.Child == null) return null;
+        	else return (SimpleBlock)SlotBorder.Child;
+        }
+    }
+}
--- trunk/Flip.UI/Window1.xaml.cs
+++ trunk/Flip.UI/Window1.xaml.cs
@@ -19,7 +19,70 @@ namespace Sussex.Flip.UI
 	{
 		public Window1()
 		{
-			InitializeComponent();
+			InitializeComponent();			
+			MainCanvas.PreviewDrop += new DragEventHandler(DroppedOnCanvas);
+			SelectionBox.PreviewDrop += new DragEventHandler(DroppedOnSelectionBox);
+		}
+		
+		
+		private int count = 1;
+		private void AddSimpleBlock(object sender, RoutedEventArgs e)
+		{
+			SimpleBlock block = new SimpleBlock(new LinearGradientBrush(Colors.Pink,Colors.Salmon,45));
+			block.Face.Content = "Block " + count++;
+			SelectionBox.Children.Add(block);
+		}
+		
+		
+		private void AddBlockWithSlots(object sender, RoutedEventArgs e)
+		{
+			BlockWithSlots block = new BlockWithSlots();
+			
+			block.AddSlot("CreatureX");
+			
+			Label label = new Label();
+			label.Content = "attacks";
+			label.VerticalAlignment = VerticalAlignment.Center;
+			label.HorizontalAlignment = HorizontalAlignment.Center;
+			block.AddLabel(label);
+			
+			block.AddSlot("CreatureY");
+			
+			SelectionBox.Children.Add(block);
+		}
+				
+
+		private void DroppedOnCanvas(object sender, DragEventArgs e)
+		{
+			IDataObject data = e.Data;
+			if (data.GetDataPresent(typeof(Moveable))) {
+				Moveable moveable = (Moveable)data.GetData(typeof(Moveable));
+				
+				Point point = e.GetPosition(MainCanvas);
+				double x = point.X - (moveable.ActualWidth/2);
+				double y = point.Y - (moveable.ActualHeight/2);
+				
+				if (!MainCanvas.Children.Contains(moveable)) {
+					moveable.Detach();
+					MainCanvas.Children.Add(moveable);
+				}					
+				
+				moveable.MoveTo(x,y);
+			}
+		}
+				
+
+		private void DroppedOnSelectionBox(object sender, DragEventArgs e)
+		{
+			IDataObject data = e.Data;
+			if (data.GetDataPresent(typeof(Moveable))) {
+				Moveable moveable = (Moveable)data.GetData(typeof(Moveable));
+								
+				if (!SelectionBox.Children.Contains(moveable)) {
+					moveable.Detach();
+					SelectionBox.Children.Add(moveable);
+				}		
+			}
 		}
 	}
 }
