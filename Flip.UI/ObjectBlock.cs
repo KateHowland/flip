12d1dc7aa554862988f91bcb18024d647ce3cea1
keiron.nicholson <keiron.nicholson@81157064-3570-11de-a9fe-ad1e22fc3a8a>
2010-02-24 11:28:23 +0000

    ObjectBlock now uses a ControlTemplate.

--- Flip.UI/ObjectBlock.cs
+++ Flip.UI/ObjectBlock.cs
@@ -18,90 +18,85 @@ namespace Sussex.Flip.UI
     /// </summary>
     public partial class ObjectBlock : Moveable
     {
-    	protected string type;
-    	protected string subtype;
-    	protected string identifier;
-    	protected string displayName;
+    	static DependencyProperty TypeProperty;
+    	static DependencyProperty SubtypeProperty;
+    	static DependencyProperty IdentifierProperty;
+    	static DependencyProperty DisplayNameProperty;
+    	static DependencyProperty DisplayImageProperty;
+    	static DependencyProperty RepresentedObjectProperty;
+    	
+    	
+    	static ObjectBlock()
+    	{
+    		TypeProperty = DependencyProperty.Register("Type",typeof(string),typeof(ObjectBlock));
+    		SubtypeProperty = DependencyProperty.Register("Subtype",typeof(string),typeof(ObjectBlock));
+    		IdentifierProperty = DependencyProperty.Register("Identifier",typeof(string),typeof(ObjectBlock));
+    		DisplayNameProperty = DependencyProperty.Register("DisplayName",typeof(string),typeof(ObjectBlock));
+    		DisplayImageProperty = DependencyProperty.Register("DisplayImage",typeof(Image),typeof(ObjectBlock));
+    		RepresentedObjectProperty = DependencyProperty.Register("RepresentedObject",typeof(object),typeof(ObjectBlock));
+    	}
+    	
+    	
+    	public string Type {
+    		get { return (string)base.GetValue(TypeProperty); }
+    		set { base.SetValue(TypeProperty,value); }
+    	}
+    	
+    	
+    	public string Subtype {
+    		get { return (string)base.GetValue(SubtypeProperty); }
+    		set { base.SetValue(SubtypeProperty,value); }
+    	}
+    	
+    	
+    	public string Identifier {
+    		get { return (string)base.GetValue(IdentifierProperty); }
+    		set { base.SetValue(IdentifierProperty,value); }
+    	}
+    	
+    	
+    	public string DisplayName {
+    		get { return (string)base.GetValue(DisplayNameProperty); }
+    		set { base.SetValue(DisplayNameProperty,value); }
+    	}
+    	
+    	
+    	public Image DisplayImage {
+    		get { return (Image)base.GetValue(DisplayImageProperty); }
+    		set { base.SetValue(DisplayImageProperty,value); }
+    	}
+    	
+    	
+    	public object RepresentedObject {
+    		get { return (object)base.GetValue(RepresentedObjectProperty); }
+    		set { base.SetValue(RepresentedObjectProperty,value); }
+    	}
+    	   	    	
+    	
     	// protected ObjectBlockBehaviour behaviour;
     	// behaviour.GetSource();
     	// behaviour.GetNaturalLanguage();
-    	protected object represented;
-    	
-    	
-		public string Type {
-			get { return type; }
-		}
-    	
-    	
-		public string Subtype {
-			get { return subtype; }
-		}
-    	
-    	
-		public string Identifier {
-			get { return identifier; }
-		}
-    	
-    	
-		public string DisplayName {
-			get { return displayName; }
-		}
-    	
-    	
-		public object Represented {
-			get { return represented; }
-		}
-    	
-    	
-		public Image Image {
-			get { 
-    			Image image = Face.Content as Image;
-    			if (image == null) throw new InvalidOperationException("Content is not a valid image.");
-    			else return image;
-    		}
-			set { 
-    			Face.Content = value;
-    		}
-		}
-    	
-    	
-    	public Border Border {
-    		get {
-    			return border;
-    		}
-    	}
     	
     	
         public ObjectBlock(Image image, object represented, string identifier, string type, string subtype, string displayName)
         {
             InitializeComponent();
             
-            Image = image;
-            this.represented = represented;
-            this.identifier = identifier;
-            this.type = type;
-            this.subtype = subtype;
+    		Type = type;
+    		Subtype = subtype;
+    		Identifier = identifier;
+    		DisplayName = displayName;
+    		DisplayImage = image;
+    		RepresentedObject = represented;
             
             ToolTip = ToString();
+            
+//            Template = (ControlTemplate)Resources["objectBlockControlTemplate"];
         }
     	
     	
         public ObjectBlock(Image image) : this(image,null,String.Empty,String.Empty,String.Empty,String.Empty)
         {
         }
-        
-        
-		public override string ToString()
-		{
-			StringBuilder sb = new StringBuilder("Name: ");
-			sb.AppendLine(displayName);
-			sb.Append("Identifier: ");
-			sb.AppendLine(identifier);
-			sb.Append("Type: ");
-			sb.AppendLine(type);
-			sb.Append("Subtype: ");
-			sb.Append(subtype);
-			return sb.ToString();
-		}
     }
 }
