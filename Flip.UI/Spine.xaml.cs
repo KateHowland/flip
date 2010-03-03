7e3c1384b6ea67f3510ea5586a5e5e419c4aa0a5
keiron.nicholson <keiron.nicholson@81157064-3570-11de-a9fe-ad1e22fc3a8a>
2010-03-03 17:22:49 +0000

    Added Spine control.

---
+++ Flip.UI/Spine.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sussex.Flip.UI
+{
+    /// <summary>
+    /// Interaction logic for Spine.xaml
+    /// </summary>
+
+    public partial class Spine : UserControl
+    {
+        public Spine(int pegs)
+        {
+            InitializeComponent();
+            for (int i = 0; i < pegs; i++) AddPeg();
+        }
+        
+        
+        public Spine() : this(1)
+        {        	
+        }
+
+        
+        public void AddPeg()
+        {
+        	Peg peg = new Peg();
+        	peg.Margin = new Thickness(0,10,0,10);
+        	pegsPanel.Children.Add(peg);
+        }
+    }
+}
