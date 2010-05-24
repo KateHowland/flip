780cbaecc597dcc967f706a7b7133bc7a518b51c
keiron.nicholson <keiron.nicholson@81157064-3570-11de-a9fe-ad1e22fc3a8a>
2010-05-24 10:08:45 +0000

    Added but have not tested IsNear.

--- Flip.NeverwinterNightsTwo.Plugin/Nwn2StatementFactory.cs
+++ Flip.NeverwinterNightsTwo.Plugin/Nwn2StatementFactory.cs
@@ -280,6 +280,12 @@ namespace Sussex.Flip.Games.NeverwinterNightsTwo
 		}
 		
 		
+		public Statement IsNear()
+		{
+			return new Statement(new IsNear(fitters));
+		}
+		
+		
 		// TODO:
 		// Won't work until code is put in to automatically include
 		// flip_functions.nss in any module.
