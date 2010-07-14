c30ec273ab1c70a0d40439faa97f9125ffbe580c
keiron.nicholson <keiron.nicholson@81157064-3570-11de-a9fe-ad1e22fc3a8a>
2010-07-14 13:24:02 +0000

    Added a Flip.NeverwinterNightsTwo.Images project.

---
+++ trunk/Flip.NeverwinterNightsTwo.Images/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sussex.Flip.Games.NeverwinterNightsTwo.Images
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+			
+			Label l = new Label();
+			
+			ImageGetter getter = new ImageGetter();
+			
+			Loaded += delegate 
+			{
+				Image myImage = getter.GetImage("Creature","c_dogwolfwin");
+				l.Content = myImage;
+				jkjkj.Children.Add(l);
+			};
+						
+			MouseDoubleClick += delegate 
+			{  
+				Image img = getter.GetImage("Creature","c_grobnar");
+				l.Content = img;
+			};
+		}
+	}
+}
